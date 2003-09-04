a1fb2d91c393dd6d8159df4dc887bcae8c74a194
Michael J. Flickinger <mjflick@gnu.org>
2003-09-04 02:20:06 +0000

    Import of MJFLICK/fldbm-0.20 from CPAN.
    gitpan-cpan-distribution: fldbm
    gitpan-cpan-version:      0.20
    gitpan-cpan-path:         MJFLICK/fldbm-0.20.tar.gz
    gitpan-cpan-author:       MJFLICK
    gitpan-cpan-maturity:     released

--- FLDBM.pm
+++ FLDBM.pm
@@ -1,10 +1,9 @@
 package FLDBM;
 
-use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.20';
 our @ISA = qw();
 
 sub GetRecord {
@@ -48,78 +47,87 @@ sub GetRecords {
 
 
 
-sub ChangeEntry {
+sub ChangeRecord {
 	my ($file,$record,$blob) = @_;
-	RecordExist($file,$record,2);
-        open(FILE, "$file");
-        my @DATA = <FILE>;
-        close(FILE);
-        my $count = 0;
-        my $line = "ERROR";
-        foreach (@DATA) {
-                my @SPLIT = split(/#%%#!#%%#/, $_);
-                if ($SPLIT[0] eq $record) {
-                        $line = $count;
-                }
-                $count++;
-        };
-        if ($line eq "ERROR") { print "Error: No such record.\n"; die };
-        my @SPLIT = split(/#%%#!#%%#/, $DATA[$line]);
-        $blob=~s/\n/#%%#NL#%%#/g;
-        $blob=~s/#%%#!#%%#//g;
-	$DATA[$line] = "\n$record#%%#!#%%#$blob\n";
-	open(FILE, ">$file");
-	print FILE @DATA;
-	close(FILE);
-	CleanDB($file);
-	return 1;
+	my $exist = RecordExist($file,$record,2);
+	if ($exist eq 'blarg') {
+		return "Error: Record does not exist.\n";
+	} else {
+	        open(FILE, "$file");
+	        my @DATA = <FILE>;
+	        close(FILE);
+	        my $count = 0;
+	        my $line = "ERROR";
+	        foreach (@DATA) {
+	                my @SPLIT = split(/#%%#!#%%#/, $_);
+	                if ($SPLIT[0] eq $record) {
+	                        $line = $count;
+	                }
+	                $count++;
+		};
+		if ($line eq "ERROR") { return "Error: No such record.\n" };
+		my @SPLIT = split(/#%%#!#%%#/, $DATA[$line]);
+		$blob=~s/\n/#%%#NL#%%#/g;
+		$blob=~s/#%%#!#%%#//g;
+		$DATA[$line] = "\n$record#%%#!#%%#$blob\n";
+		open(FILE, ">$file");
+		print FILE @DATA;
+		close(FILE);
+		CleanDB($file);
+		return 1;
+	}
 };
 
 
 
-sub DeleteEntry {
+sub DeleteRecord {
         my ($file,$record,$blob) = @_;
-        RecordExist($file,$record,2);
-        open(FILE, "$file");
-        my @DATA = <FILE>;
-        close(FILE);
-        my $count = 0;
-        my $line = "ERROR";
-        foreach (@DATA) {
-                my @SPLIT = split(/#%%#!#%%#/, $_);
-                if ($SPLIT[0] eq $record) {
-                        $line = $count;
-                }
-                $count++;
-        };
-        if ($line eq "ERROR") { print "Error: No such record.\n"; die };
-        my @SPLIT = split(/#%%#!#%%#/, $DATA[$line]);
-        $DATA[$line] = "";
-        open(FILE, ">$file");
-        print FILE @DATA;
-        close(FILE);
-        CleanDB($file);
-        return 1;
-};
+        my $exist = RecordExist($file,$record,2);
+	if ($exist eq 'blarg') {
+		return "Error: Record does not exist.\n";
+	} else {
+	        open(FILE, "$file");
+	        my @DATA = <FILE>;
+	        close(FILE);
+	        my $count = 0;
+	        my $line = "ERROR";
+	        foreach (@DATA) {
+	                my @SPLIT = split(/#%%#!#%%#/, $_);
+	                if ($SPLIT[0] eq $record) {
+	                        $line = $count;
+	       	        }
+        	        $count++;
+	        };
+	        if ($line eq "ERROR") { return "Error: No such record.\n" };
+	        my @SPLIT = split(/#%%#!#%%#/, $DATA[$line]);
+        	$DATA[$line] = "";
+	        open(FILE, ">$file");
+	        print FILE @DATA;
+	        close(FILE);
+	        CleanDB($file);
+	        return 1;
+	}
+}
 
 
-
-
-sub AddEntry {
+sub AddRecord {
         my ($file,$record,$blob) = @_;
 	system("touch", $file);
 	$record=~s/\n/#%%#NL#%%#/g;
 	$blob=~s/\n/#%%#NL#%%#/g;
         $record=~s/#%%#!#%%#//g;
         $blob=~s/#%%#!#%%#//g;
-	RecordExist($file,$record,1);
-	open(FILE, ">>$file");
-        print FILE "\n$record#%%#!#%%#$blob";
-	close(FILE);
-	CleanDB($file);
-        return 1;
-};
-
+	my $exist = RecordExist($file,$record,1);
+	if ($exist eq 'blarg') {
+		return "Error: Record already exists.\n";
+	} else {
+		open(FILE, ">>$file");
+	        print FILE "\n$record#%%#!#%%#$blob";
+		close(FILE);
+		CleanDB($file);
+	        return 1;
+	}
+}
 
 
 sub RecordExist {
@@ -130,7 +138,7 @@ sub RecordExist {
 	if ($check eq 1) {
         	foreach (@DATA) {
 			my @SPLIT = split(/#%%#!#%%#/, $_);
-			if ($SPLIT[0] eq $record) { print "Error: Record already exists.\n"; die };
+			if ($SPLIT[0] eq $record) { return 'blarg' };
 		};
 	}
 	if ($check eq 2) {
@@ -139,12 +147,10 @@ sub RecordExist {
 	                my @SPLIT = split(/#%%#!#%%#/, $_);
 	                if ($SPLIT[0] eq $record) { $checkvar = 1 };
 		}
-		if ($checkvar eq 0) { print "Error: Record does not exists.\n"; die };
+		if ($checkvar eq 0) { return 'blarg' };
 	}
 	return 1;
-};
-
-
+}
 
 
 sub CleanDB {
@@ -164,7 +170,7 @@ sub CleanDB {
         print FILE @DATA;
         close(FILE);
 	return 1;
-};
+}
 
 
 1;
@@ -180,29 +186,19 @@ FLDBM
 
   use FLDBM;
 
-  FLDBM::GetRecord(file,record);          Returns information from a record.
-  FLDBM::GetRecords(file);                Returns list of records from file.
-  FLDBM::ChangeEntry(file,record,blob)    Changes a record.
-  FLDBM::DeleteEntry(file,record)         Deletes a record.
-  FLDBM::AddEntry(file,record,blob)       Adds a record.
+  FLDBM::GetRecord(file,record);           Returns information from a record.
+  FLDBM::GetRecords(file);                 Returns list of records from file.
+  FLDBM::ChangeRecord(file,record,blob)    Changes a record.
+  FLDBM::DeleteRecord(file,record)         Deletes a record.
+  FLDBM::AddRecord(file,record,blob)       Adds a record.
   
 
 =head1 DESCRIPTION
 
 FLDBM is a textfile database storage system.  It allows for a feild name and value to be stored in a textfile and later recalled.
 
-=head2 EXPORT
-
-None.
-
-=head1 SEE ALSO
-
 =head1 AUTHOR
 
 Michael J. Flickinger, E<lt>mjflick@cpan.orgE<gt>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2003 by Michael J. Flickinger
 
 =cut
